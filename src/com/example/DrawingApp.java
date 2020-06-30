cf81ee514c09b018e2a57547ef5dfa0f146dcedd
Sahitya <sahityakmr28@gmail.com>
2020-07-01 04:09:20 +0530

    Using Bean Factory

--- src/com/example/DrawingApp.java
+++ src/com/example/DrawingApp.java
@@ -1,8 +1,14 @@
 package com.example;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
 public class DrawingApp {
     public static void main(String[] args) {
-        Triangle triangle = new Triangle();
+        //Triangle triangle = new Triangle();
+        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+        Triangle triangle = (Triangle) factory.getBean("triangle");
         triangle.draw();
     }
 }
