192bced60801fed9adeb5369e42eaa492b0501f1
Sahitya <sahityakmr28@gmail.com>
2020-07-05 05:45:37 +0530

    Using Constructor Injection

--- src/com/example/Triangle.java
+++ src/com/example/Triangle.java
@@ -1,7 +1,36 @@
 package com.example;
 
 public class Triangle {
+    private String type;
+    private int height;
+
+    public Triangle(String type) {
+        this.type = type;
+        this.height = 0;
+    }
+
+    public Triangle(String type, int height) {
+        this.type = type;
+        this.height = height;
+    }
+
+    public Triangle(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    /*public void setType(String type) {
+        this.type = type;
+    }*/
+
     public void draw() {
-        System.out.println("Drawing Triangle");
+        System.out.println("Drawing " + type + " Triangle with height " + getHeight());
     }
 }
