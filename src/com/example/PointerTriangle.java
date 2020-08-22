2a5c5614d4032393bf31a72eddeb76e100f08612
sahityakmr28@gmail.com <Devl3@git>
2020-08-23 02:12:43 +0530

    ApplicationContextAware, BeanNameAware

--- src/com/example/PointerTriangle.java
+++ src/com/example/PointerTriangle.java
@@ -1,9 +1,16 @@
 package com.example;
 
-public class PointerTriangle {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class PointerTriangle implements ApplicationContextAware, BeanNameAware {
     private Point pointA;
     private Point pointB;
     private Point pointC;
+    private ApplicationContext applicationContext;
+    private String beanName;
 
     public PointerTriangle(Point pointA, Point pointB, Point pointC) {
         this.pointA = pointA;
@@ -40,5 +47,17 @@ public class PointerTriangle {
 
     public void draw() {
         System.out.printf("Drawing Triangle with points %s, %s, %s%n", pointA, pointB, pointC);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Capturing Application Context!!");
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.printf("Bean Name is : %s%n", name);
+        this.beanName = name;
     }
 }
